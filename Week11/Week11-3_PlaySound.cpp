3e22d00b698a9f288c96c21e1b34dc2643963cf3
Wednesday1112 <s1014220@gmail.com>
2023-04-27 16:32:22 +0800

    Add

---
+++ Week11/Week11-3_PlaySound.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+#include <stdio.h>
+int main()
+{
+    ///PlaySound("C:\\Users\\Administrator\\Desktop\\do-re-mi\\do.wav",NULL,SND_SYNC);
+    PlaySound("do-re-mi/do.wav",NULL,SND_SYNC);
+    printf("Hello World!");
+}
